414f3e6a4626799449d035c0eb9d55e92cf1e369
ShiChangshan <changshanshi@outlook.com>
2024-12-18 10:59:15 +0800

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import sharklog
-
-sharklog.init(debug=True)  # init current logger with level=sharklog.DEBUG
-
-sharklog.debug("debug message")
-
-sharklog.info("info message")
-sharklog.warning("warning message")
-sharklog.error("error message")
